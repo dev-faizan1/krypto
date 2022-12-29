b5a36c827725d9c4a1b89aceb7334d05d3302f46
dev-faizan1 <ch.faizan1947@gmail.com>
2022-12-29 14:03:16 +0500

    done

---
+++ lib/coindata.dart
@@ -0,0 +1,21 @@
+List<String> currencies = [
+    'USD',
+    'EUR',
+    'GBP',
+    
+  ];
+
+
+
+List<String> cryptoCoins = [
+    'BTC',
+    'ETH',
+    'LiteCoin',
+    'Crazy Miner',
+  ];
+
+
+
+class COinData {
+  
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'networ.dart';
+import 'coindata.dart';
+import 'dart:io' show Platform;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -12,6 +14,48 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String currentPrice = '??';
+  String? selectedCurrency = 'USD';
+
+  //DropDownBtn
+  DropdownButton dropDownBtn() {
+    List<DropdownMenuItem<String>> dropDownList = [];
+
+    for (String currency in currencies) {
+      var newItem = DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      );
+      dropDownList.add(newItem);
+    }
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropDownList,
+      onChanged: ((value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      }),
+    );
+  }
+
+  //CupertinoPicker
+  CupertinoPicker cupertinoPicker() {
+    List<Widget> pickerItems = [];
+    for (String currency in currencies) {
+      var item = Text(currency);
+      pickerItems.add(item);
+    }
+
+    return CupertinoPicker(
+      itemExtent: 30,
+      onSelectedItemChanged: (selectedIndex) {},
+      children: pickerItems,
+    );
+  }
+
+  
+
   void fetchData() async {
     try {
       double data = await NetWork().getPrice();
@@ -35,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Scaffold(
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 alignment: Alignment.center,
@@ -44,7 +88,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.red,
                 child: Text('BTC price in USD is $currentPrice'),
               ),
-              ElevatedButton(onPressed: fetchData, child: Text('Update price')),
+              Container(
+                  color: Colors.blueAccent,
+                  height: 100,
+                  alignment: Alignment.center,
+                  child: Platform.isIOS? cupertinoPicker():dropDownBtn()),
             ],
           ),
         ),
